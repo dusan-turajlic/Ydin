f1cf322f8b91b4abab83c859e0ef47bf7e75695e
Dusan Turajlic <dusan.turajlic@cambri.io>
2026-01-08 18:16:51 +0200

    Fixes the iPhone top notch problem

--- packages/Nutrition/src/views/Tracker.tsx
+++ packages/Nutrition/src/views/Tracker.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-import { useAtomValue } from "jotai";
 import { useParams } from "react-router-dom";
 import WeekDaySelector from "@/components/WeekDaySelector";
 import TopNavigation from "@/components/TopNavigation";
@@ -7,14 +6,12 @@ import DiaryTracker from "@/components/DiaryTracker";
 import MacroProgressBar from "@/components/MacroProgressBar";
 import FoodSearchSheet from "@/components/FoodSearchSheet";
 import { LoadingSpinner } from "@/components/ui";
-import { sheetExpandedAtom } from "@/atoms/sheet";
 
 export default function Tracker() {
     const { code } = useParams();
-    const isExpanded = useAtomValue(sheetExpandedAtom);
 
     return (
-        <div className={`flex flex-col gap-1 max-w-96 m-auto ${isExpanded ? 'overflow-hidden h-screen' : 'min-h-screen'}`}>
+        <div className="flex flex-col gap-1 max-w-96 m-auto">
             <TopNavigation />
             <WeekDaySelector />
             <MacroProgressBar />
